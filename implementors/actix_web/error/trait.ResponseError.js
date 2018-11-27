7e434c5335ddd7c2fbcead8c83d4437b93b8ded8
app-services-devs <application-services@mozilla.com>
2018-11-27 18:07:53 +0000

    chore: rebuild developer docs

--- implementors/actix_web/error/trait.ResponseError.js
+++ implementors/actix_web/error/trait.ResponseError.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["actix_web"] = [];
-implementors["syncstorage"] = [{text:"impl <a class=\"trait\" href=\"actix_web/error/trait.ResponseError.html\" title=\"trait actix_web::error::ResponseError\">ResponseError</a> for <a class=\"struct\" href=\"syncstorage/error/struct.ApiError.html\" title=\"struct syncstorage::error::ApiError\">ApiError</a>",synthetic:false,types:["syncstorage::error::ApiError"]},{text:"impl <a class=\"trait\" href=\"actix_web/error/trait.ResponseError.html\" title=\"trait actix_web::error::ResponseError\">ResponseError</a> for <a class=\"struct\" href=\"syncstorage/db/error/struct.DbError.html\" title=\"struct syncstorage::db::error::DbError\">DbError</a>",synthetic:false,types:["syncstorage::db::error::DbError"]},];
+implementors["syncstorage"] = [{text:"impl <a class=\"trait\" href=\"actix_web/error/trait.ResponseError.html\" title=\"trait actix_web::error::ResponseError\">ResponseError</a> for <a class=\"struct\" href=\"syncstorage/error/struct.ApiError.html\" title=\"struct syncstorage::error::ApiError\">ApiError</a>",synthetic:false,types:["syncstorage::error::ApiError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
